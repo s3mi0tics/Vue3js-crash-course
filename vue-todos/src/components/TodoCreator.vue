a0d663b0d64b43a2679f077b4ada5c33313b7dc4
s3miOtics <colby.kauk@gmail.com>
2024-01-12 00:15:30 -0800

    make TodoCreator component with emit and v-show

---
+++ vue-todos/src/components/TodoCreator.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { reactive } from "vue";
+
+const emit = defineEmits(["create-todo"]);
+
+const todoState = reactive({
+  todo:"",
+  invalid: null,
+  errMsg: "",
+});
+
+const createTodo = () => {
+  todoState.invalid = null;
+  if (todoState.todo !== "") {
+    emit("create-todo", todoState.todo);
+    todoState.todo = "";
+    return;
+  }
+  todoState.invalid = true;
+  todoState.errMsg = "Todo value cannot be empty"
+
+};
+ </script>
+<template>
+    <div class="input-wrap">
+        <input type="text" v-model="todoState.todo" />
+        <button @click="createTodo()">Create</button>
+    </div>
+    <p v-show="todoState.invalid" class="err-msg">{{ todoState.errMsg }}</p>
+</template>
+
+<style lang="scss" scoped>
+.input-wrap {
+  display: flex;
+  transition: 250ms ease;
+  border: 2px solid #41b080;
+  border-radius: 3px;
+
+  &:focus-within {
+    box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+      0 -2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+}
+</style>
