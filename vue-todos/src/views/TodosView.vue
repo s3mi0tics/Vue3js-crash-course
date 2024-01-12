7aa3c786eff5c92d92b968a58ccf630f00d4ea89
s3miOtics <colby.kauk@gmail.com>
2024-01-12 09:38:49 -0800

    add styles and conditional rendering (v-if, v-else)

--- vue-todos/src/views/TodosView.vue
+++ vue-todos/src/views/TodosView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { uid } from "uid";
 import { ref } from "vue";
+import { Icon } from '@iconify/vue'
 import TodoCreator from "../components/TodoCreator.vue";
+import TodoItem from "../components/TodoItem.vue";
 const todoList = ref([]);
 const createTodo = (todo) => {
   todoList.value.push({
@@ -17,6 +19,13 @@ const createTodo = (todo) => {
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo" />
+    <ul v-if="todoList.length > 0" class="todo-list">
+      <TodoItem v-for="todo in todoList" :todo="todo" />
+    </ul>
+    <p v-else class="todos-msg">
+      <Icon icon="nato-v1-:sad-but-relieved-face" width="22" />
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
   </main>
 </template>
 
@@ -32,6 +41,20 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
